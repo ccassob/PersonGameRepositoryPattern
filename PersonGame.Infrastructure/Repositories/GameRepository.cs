50ff41c82e48528cbb48298481ceacdb7f845cdd
gaperaltav <gperalta@wind.com.do>
2018-10-12 17:00:10 -0400

    Extendiendo funcionalidad del Repositorio Generico a GameRepository

---
+++ PersonGame.Infrastructure/Repositories/GameRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonGame.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using PersonGame.Domain;
+
+namespace PersonGame.Infrastructure.Repositories
+{
+    public class GameRepository : EfRepository
+    {
+        private readonly DbContext _dbContext;
+
+        public GameRepository(AppDbContext dbContext)
+            : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Game> GetByGenre(string name, string gendre)
+        {
+            return this.Get<Game>(g => g.Genre == name);
+        }
+    }
+}
