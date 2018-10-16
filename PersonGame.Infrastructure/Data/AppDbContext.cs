f2cfbc48fb12ec46f31e8615ab65aa951c9268ec
Christopher <ccasso@wind.com.do>
2018-10-16 12:02:40 -0400

    Quitado codigo de mas

--- PersonGame.Infrastructure/Data/AppDbContext.cs
+++ PersonGame.Infrastructure/Data/AppDbContext.cs
@@ -9,22 +9,6 @@ namespace PersonGame.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
-        //public AppDbContext()
-        //{
-            
-        //    //var connection = @"Data Source=.;Initial Catalog=AppDatabase;Integrated Security=True;";
-        //    //services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
-
-        //    //var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        //    //optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=AppDatabase;Integrated Security=True;");
-
-        //}
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(@"Data Source=.;Initial Catalog=AppDatabase;Integrated Security=True;");
-        }
-
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
